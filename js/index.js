f88889a0abdcb67ca5dfb267829e32bd491278a9
sourav sharma <souravsh1234567@gmail.com>
2021-12-05 21:40:21 +0530

    added DarkMode

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 const header_height = 95 ;
 const list_height = 150 ;
 let checkbox = document.querySelector(".bar input[type='checkbox']");
+const themButton = document.querySelector("#theam-button");
+themButton.addEventListener("click",(button)=>{
+    if(themButton.checked){
+        // night mode
+        changetheam(themButton.checked);
+    }else {
+        // light theamm mode 
+        changetheam(themButton.checked);
+
+    }
+});
 const header = {
     header : document.querySelector(".header"),
     list : document.querySelector(".header nav")
@@ -239,11 +250,12 @@ lets_contact.addEventListener("click",()=>{
         form_visible = true ;
 });
 
-
+// Navigation buttor click event setting up
 nav_buttons.forEach((element,index)=>{
-    if(nav_buttons.length-1 === index){
+    if(nav_buttons.length-2 === index){
 
         element.addEventListener("click",()=>{
+            console.log("click");
             disposable.style.display = "none";
             form.style.display="flex";
             form_visible = true ;
@@ -262,4 +274,150 @@ nav_buttons.forEach((element,index)=>{
     
 });
 
+
+function changetheam(mode){
+    const colour = theamColour(mode);
+    const header =  getHeader();
+    const body = document.querySelector("body");
+    const projectContainer = document.querySelector(".my_projects_container");
+    const lets_contact = document.querySelector(".lets_contact");
+    const card = document.querySelectorAll(".my_projects_container_item");
+    const content  =  document.querySelectorAll(".show_content_flex p");
+    const about  = document.querySelectorAll(".about_me_content");
+    const contact = document.querySelector(".lets_contact_headings");
+    const contact_button = document.querySelectorAll(".lets_contact_social_media_section_button");
+    const contact_button_a = document.querySelectorAll(".lets_contact_social_media_section_button a");
+    const footer  = document.querySelector(".footer");
+    const form_box = document.querySelector(".form_box_img");
+    const form_box_boxing = document.querySelector(".form_box_boxing");
+    const form_box_boxing_label = document.querySelectorAll(".form_box_boxing label");
+    const form_box_boxing_input = document.querySelectorAll(".form_box_boxing input");
+    const form_box_boxing_textarea = document.querySelector("#message");
+
+
+    form_box.style.background = colour.$bodybackground ;
+    form_box_boxing.style.background = colour.$bodybackground;
+
+    form_box_boxing_label.forEach((item)=>{
+        item.style.color = colour.$textcolour ; 
+    });
+    form_box_boxing_input.forEach((item)=>{
+
+        item.style.background = colour.$inputbackground;
+        item.style.color = colour.$textcolour;
+    });
+    form_box_boxing_textarea.style.background = colour.$inputbackground;
+    form_box_boxing_textarea.style.color = colour.$textcolour;
+
+    body.style.backgroundColor = colour.$bodybackground;
+    header.header.style.background = colour.$PinkColour ;
+    projectContainer.style.backgroundColor = colour.$backgroundcolour;
+    lets_contact.style.backgroundColor = colour.$backgroundcolour ;
+
+
+    card.forEach((item,index)=>{
+        item.style.backgroundColor = colour.$bodybackground ;
+            item.style.color = colour.$textcolour ;
+     
+    });
+    header.headerlist.forEach((item,index ,list)=>{
+       if(list.length -1 != index){
+       item.style.color = colour.$headertextcolour;
+    }
+   });
+   content.forEach((item)=>{
+
+    item.style.color = colour.$textcolour ;
+   });
+   about.forEach((item)=>{
+        item.style.color = colour.$textcolour;
+   });
+   contact.style.color = colour.$textcolour;
+
+   contact_button.forEach((item,index)=>{
+    item.style.color = colour.$buttontext;
+
+   });
+
+   contact_button_a.forEach((item)=>{
+       item.style.color = colour.$buttontext;
+
+   });
+
+   footer.style.color = colour.$textcolour;
+
+
+
+}
+function getHeader(){
+    return {
+        header : document.querySelector(".header"),
+        headerlist : document.querySelectorAll(".header nav ul li a")
+    }
+}
+
+
+function theamColour(mode){
+
+//    #15223C
+// $yellowColour : #f58a07 ;
+// $ButtonColour : #ddd6fe ;
+// $cardButton : #DDD6fE ;
+// $cardButtonHover : #ffacb7 ;
+// $readmeHOver : #6b88c6;
+// $findmorecolour : #6886C5 ;
+// $backgroundcolour : #F5F5F5 ;
+// $hoverlightbluecolour : #6886c5;
+// $hoverdarkbluecolour : #1e40af;
+// $formBackgroundcolour : #6886C5;
+    if(mode){
+        // 1c2d50
+        // night mode
+        return {
+            $textcolour : "#FFF",
+            $PinkColour :  "#1E5F74",
+            $yellowColour : "#f58a07" ,
+            $ButtonColour : "#FCDAB7" ,
+            $cardButton : "#FCDAB7" ,
+            $cardButtonHover : "#fec576" ,
+            $readmeHOver : "#e1686b",
+            $findmorecolour : "#1e5f81" ,
+            $backgroundcolour : "#15223C" ,
+            $hoverlightbluecolour : "#6886c5",
+            $hoverdarkbluecolour : "#1e40af" ,
+            $formBackgroundcolour : "#6886C5" ,
+            $headertextcolour : "#fff",
+            $bodybackground : "#1c2d50" ,
+            $buttontext : "#1c2d50",
+            $buttonbackgroud : "#fcdab7" ,
+            $inputbackground : "#3b3b3b"
+        }
+        
+    }else {
+        // light mdoe
+        return {
+            $textcolour : "#000",
+            $PinkColour :  "#ffacb7",
+            $yellowColour : "#f58a07" ,
+            $ButtonColour : "#ddd6fe" ,
+            $cardButton : "#DDD6fE" ,
+            $cardButtonHover : "#ffacb7" ,
+            $readmeHOver : "#6b88c6",
+            $findmorecolour : "#6886C5" ,
+            $backgroundcolour : "#F5F5F5" ,
+            $hoverlightbluecolour : "#6886c5",
+            $hoverdarkbluecolour : "#1e40af" ,
+            $formBackgroundcolour : "#6886C5" ,
+            $headertextcolour : "#000",
+            $bodybackground : "#f9f9f9",
+            $buttontext : "#000" ,
+            $buttonbackgroud : "#ffacb7" ,
+            $inputbackground : "#fff"
+
+
+           
+
+        }
+    }
+}
 
